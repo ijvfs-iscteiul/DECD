1dff02cca6a4986b699d193bb83d8655fb5e15c0
Ben-Hur Fidalgo <ben.hur.fidalgo@jmellosaude.pt>
2021-05-18 18:42:43 +0100

    + Working example extracting csv data to an array

--- src/Cenas.js
+++ src/Cenas.js
@@ -3,36 +3,37 @@ import csv from 'csv-parser';
 import fs from 'fs';
 import { convertCSVToArray } from 'convert-csv-to-array';
 
+const pathToBaseFile = 'docs/Formandos_Terminados2018_8.csv'
+
 const convertFile = async () => {
 	console.log('Initing script');
-	debugger;
-	await fs.createReadStream('data.csv')
-		.pipe(streamedData => {
-			try {
-				debugger;
-				const pow = csv(streamedData);
-				debugger;
-				return pow;
-			} catch (error) {
-				debugger;
-				console.error(error);
-			}
-		})
-		.on('data', (row) => {
-			debugger;
-			console.log(row);
-		})
-		.on('end', () => {
-			debugger;
-			console.log('CSV file successfully processed');
-		});
-	debugger;
+	const data = [];
+	try {
+		await fs.createReadStream(pathToBaseFile, 'utf-8')
+			.pipe(csv())
+			.on('data', (row) => {
+				data.push(row);
+			})
+			.on('end', () => {
+				const cenas = data
+				console.log('CSV file successfully processed');
+				// try {
+				// 	debugger;
+				// 	const arrayofArrays = convertCSVToArray(data, {
+				// 		type: 'array',
+				// 		separator: ';', // use the separator you use in your csv (e.g. '\t', ',', ';' ...)
+				// 	});
+				// } catch (error) {
+				// 	debugger;
+				// 	console.error(error)
+				// }
+			});
+	} catch (error) {
+		debugger;
+		console.error(error);
+	}
 	const formandosFile = {};
 
-	/* const arrayofArrays = convertCSVToArray(data, {
-		type: 'array',
-		separator: ';', // use the separator you use in your csv (e.g. '\t', ',', ';' ...)
-	}); */
 
 };
 
